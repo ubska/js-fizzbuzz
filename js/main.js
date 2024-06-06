f6e49689f6b0c458612b6b71ab9a46b4104a6515
Oleksandra Ubska <alessandraubska@gmail.com>
2024-06-06 16:51:55 +0200

    individuare fizz e buzz

--- js/main.js
+++ js/main.js
@@ -5,5 +5,13 @@
 
 // Scrivi un programma che stampi in console i numeri da 1 a 100,
 for (let i = 1; i <= 100; i++){
-    console.log(i);
+    if (i % 3 == 0) {
+        console.log("Fizz");
+    }else if (i % 5 == 0) {
+        console.log("Buzz");
+    }else if(i % 3 == 0 && i % 5 == 0) {
+        console.log(FlizBuzz);
+    } else {
+        console.log(i);
+    }
 }
